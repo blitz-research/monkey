400f56e6d7ab87c601738f327b3059cbf8a22eef
blitz-research <blitzmunter@gamil.com>
2013-06-06 14:48:54 +1200

    Updated brl.thread.

--- modules/brl/native/thread.cs
+++ modules/brl/native/thread.cs
@@ -1,11 +1,13 @@
 
 class BBThread{
 
+	private Object _result;
 	private bool _running;
 	private Thread _thread;
 	
 	public virtual void Start(){
 		if( _running ) return;
+		_result=null;
 		_running=true;
 		_thread=new Thread( new ThreadStart( this.run ) );
 		_thread.Start();
@@ -13,6 +15,14 @@ class BBThread{
 	
 	public virtual bool IsRunning(){
 		return _running;
+	}
+	
+	public Object Result(){
+		return _result;
+	}
+	
+	public void SetResult( Object result ){
+		_result=result;
 	}
 
 	public virtual void Run__UNSAFE__(){
