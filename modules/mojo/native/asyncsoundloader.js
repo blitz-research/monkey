1e2e3b24655e9d6eec862f5f3fe971e04cd8989c
blitz-research <blitzmunter@gamil.com>
2014-03-10 12:25:59 +1300

    Updated html5 async audio loader.

--- modules/mojo/native/asyncsoundloader.js
+++ modules/mojo/native/asyncsoundloader.js
@@ -1,11 +1,41 @@
 
 function BBAsyncSoundLoaderThread(){
+	this._running=false;
 }
-
+  
 BBAsyncSoundLoaderThread.prototype.Start=function(){
-	this._sample=this._device.LoadSample( this._path );
-}
 
+	this._sample=null;
+	if( !this._device.okay ) return;
+	
+	var audio=new Audio();
+	if( !audio ) return;
+	
+	var thread=this;
+	thread._running=true;
+
+	audio.src=BBGame.Game().PathToUrl( this._path );
+	audio.preload='auto';	
+	
+	var success=function( e ){
+		thread._sample = new gxtkSample( audio );
+		thread._running=false;
+		audio.removeEventListener( 'canplaythrough',success,false );
+		audio.removeEventListener( 'error',error,false );
+	}
+	
+	var error=function( e ){
+		thread._running=false;
+		audio.removeEventListener( 'canplaythrough',success,false );
+		audio.removeEventListener( 'error',error,false );
+	}
+	
+	audio.addEventListener( 'canplaythrough',success,false );
+	audio.addEventListener( 'error',error,false );
+	
+	audio.load();
+  }
+  
 BBAsyncSoundLoaderThread.prototype.IsRunning=function(){
-	return false;
+	return this._running;
 }
