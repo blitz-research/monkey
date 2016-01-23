e79cba92264e55f213a1c2839c073037d7f8697e
Anthony Diamond <sonickid8@live.com>
2016-01-23 08:11:02 -0700

    Fixed window closing behavior with GLFW3+ANGLE. (Duplicate code)

--- targets/glfw3_angle/modules/native/glfwgame.cpp
+++ targets/glfw3_angle/modules/native/glfwgame.cpp
@@ -580,6 +580,8 @@ void BBGlfwGame::OnCursorPos( GLFWwindow *window,double x,double y ){
 void BBGlfwGame::OnWindowClose( GLFWwindow *window ){
 	_glfwGame->KeyEvent( BBGameEvent::KeyDown,0x1b0 );
 	_glfwGame->KeyEvent( BBGameEvent::KeyUp,0x1b0 );
+	
+	glfwSetWindowShouldClose(_glfwGame->_window, 0); // GL_FALSE
 }
 
 void BBGlfwGame::OnWindowSize( GLFWwindow *window,int width,int height ){
